02e2ae45a2349e3c8dcac7a0b3735ac4221a455d
Zhong98 <91147077+Zhong98@users.noreply.github.com>
2022-05-20 01:32:06 +0200

    JDBC/JavaWeb Projet

---
+++ test10_18.sql
@@ -0,0 +1,72 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 50711
+ Source Host           : localhost:3306
+ Source Schema         : test10_18
+
+ Target Server Type    : MySQL
+ Target Server Version : 50711
+ File Encoding         : 65001
+
+ Date: 20/05/2022 00:46:24
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for customers
+-- ----------------------------
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE `customers`  (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `balance` int(11) NULL DEFAULT NULL,
+  `limit` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of customers
+-- ----------------------------
+INSERT INTO `customers` VALUES (1, 'XiaoWang', '123456', 15000, 5000);
+INSERT INTO `customers` VALUES (2, 'XiaoZhang', '654321', 25000, 10000);
+
+-- ----------------------------
+-- Table structure for products
+-- ----------------------------
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products`  (
+  `id` int(11) NOT NULL,
+  `price` int(11) NULL DEFAULT NULL,
+  `stock` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of products
+-- ----------------------------
+INSERT INTO `products` VALUES (1, 100, 10);
+INSERT INTO `products` VALUES (2, 500, 20);
+
+-- ----------------------------
+-- Table structure for sale_log
+-- ----------------------------
+DROP TABLE IF EXISTS `sale_log`;
+CREATE TABLE `sale_log`  (
+  `customerId` int(11) NULL DEFAULT NULL,
+  `productId` int(11) NULL DEFAULT NULL,
+  `count` int(11) NULL DEFAULT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `totalPrice` int(11) NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sale_log
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
